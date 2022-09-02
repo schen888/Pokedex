f9d19a9d90f5ae434bf80f81d7a837c318a5eb34
schen888 <shuang.chen888@hotmail.com>
2022-09-02 19:55:11 +0200

    Add pokemoneList array and add the first three pokemons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-alert('Hello world');
-let favoriteFood='dumplings';
-document.write(favoriteFood);   
+let pokemonList=[
+    pokemon1,
+    pokemon2,
+    pokemon3
+];
+
+let pokemon1={
+    name:'Bulbasaur',
+    height:0.7,
+    type: ['Grass','Poison']
+};
+
+let pokemon2={
+    name:'Ivysaur',
+    height:1,
+    type: ['Grass','Poison']
+};
+
+let pokemon3={
+    name:'Venusaur',
+    height:2,
+    type: ['Grass','Poison']
+};
